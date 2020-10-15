ff0d91bb47e9cc949d466da6bc476e2515fa5ad2
gurelbs <guri240@gmail.com>
2020-10-15 12:39:06 +0300

    Updates

---
+++ precache-manifest.0d3393ef273f2a63f5a900b749f49d22.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e0cc73eb47a308b0d8e1ae2d6e26ea05",
+    "url": "/REACT/index.html"
+  },
+  {
+    "revision": "ec4790bb34b040efd6bf",
+    "url": "/REACT/static/css/main.d8e93136.chunk.css"
+  },
+  {
+    "revision": "122179f5a4afa75e2459",
+    "url": "/REACT/static/js/2.6a25ef39.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/REACT/static/js/2.6a25ef39.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ec4790bb34b040efd6bf",
+    "url": "/REACT/static/js/main.1be8f74a.chunk.js"
+  },
+  {
+    "revision": "f7c9168709cf1cafe9f0",
+    "url": "/REACT/static/js/runtime-main.98304ae8.js"
+  }
+]);
